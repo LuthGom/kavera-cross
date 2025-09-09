fc9ae638a3987a13cc93deab9ef85e81cc2e9fd7
LuthGom <lucianomendes23@yahoo.com>
2025-09-09 19:45:29 -0300

    fix: ajustando informações

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -9,5 +10,6 @@ export default defineConfig({
     tailwind({
       configFile: "./tailwind.config.mjs",
     }),
+    icon(),
   ],
 });
